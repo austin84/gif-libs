77ad0521628b70488c5e1e2a9caa44eacb2ad061
austin84 <austinveatch84@gmail.com>
2020-10-12 10:09:26 -0500

    test commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div>
+      <header className='Header'>
+        <h1>GIF-Libs</h1>
+        <h2>Sample ||| Nav</h2>
+      </header>
+    </div>
+  );
+}
