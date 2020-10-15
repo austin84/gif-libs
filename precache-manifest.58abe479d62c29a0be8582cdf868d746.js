47cfe41832e50c88df5cda7f32f27254821375bf
austin84 <austinveatch84@gmail.com>
2020-10-15 06:46:51 -0500

    Updates

---
+++ precache-manifest.58abe479d62c29a0be8582cdf868d746.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef563eaf12d0b18701dbc44c4932be17",
+    "url": "/austin84/gif-libs/index.html"
+  },
+  {
+    "revision": "b7f2ccb9643d080cbb92",
+    "url": "/austin84/gif-libs/static/css/main.d1ae7263.chunk.css"
+  },
+  {
+    "revision": "32f2f4bf7acaab6e9ee6",
+    "url": "/austin84/gif-libs/static/js/2.d8ac9028.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/austin84/gif-libs/static/js/2.d8ac9028.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f2ccb9643d080cbb92",
+    "url": "/austin84/gif-libs/static/js/main.40077953.chunk.js"
+  },
+  {
+    "revision": "6534bd964a5f359e2221",
+    "url": "/austin84/gif-libs/static/js/runtime-main.06b553e7.js"
+  }
+]);
